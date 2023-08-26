68b7d32c2944810ba2c18e2d7c7928dc56548f25
abhishek lohani <alohani1308@gmail.com>
2023-08-26 16:45:18 +0530

    adding play pause button

--- samespaceui/src/components/Music/Music.js
+++ samespaceui/src/components/Music/Music.js
@@ -1,63 +1,84 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import '../../App.css';
 
-function Music( { id,searchData } ) {
+function Music({ id, searchData }) {
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [currentTime, setCurrentTime] = useState(0);
 
-    console.log(id);
-    console.log(searchData);
+    const audioRef = useRef(null);
 
-    const t=searchData.filter((d)=>Number(d.id) === Number(id));
+    const t = searchData.filter((d) => Number(d.id) === Number(id));
 
-   console.log(t)
+    const handleBackPress = () => {
+        if (audioRef.current) {
+            audioRef.current.currentTime -= 10; // Go back 10 seconds
+        } 
+    };
+
+    const handlePlayPause = () => {
+        if (audioRef.current) {
+            if (isPlaying) {
+                audioRef.current.pause();
+            } else {
+                audioRef.current.play();
+            }
+            setIsPlaying(!isPlaying);
+        }
+    };
+
+    const handleForwardPress = () => {
+        if (audioRef.current) {
+            audioRef.current.currentTime += 10; // Go forward 10 seconds
+        }
+    };
+
+    const handleTimeUpdate = () => {
+        if (audioRef.current) {
+            setCurrentTime(audioRef.current.currentTime);
+        }
+    };
 
     return (
-            <div className='music--player'>
+        <div className='music--player'>
             {t.map((d) => (
                 <div className='music--card' key={d.id}>
-                <div className='music--info'>
-                    <div className='music--details'>
-                    <div className='music--title'>{d.name}</div>
-                    <div className='music--artist'>{d.artist}</div>
+                    <div className='music--info'>
+                        <div className='music--details'>
+                            <div className='music--title'>{d.name}</div>
+                            <div className='music--artist'>{d.artist}</div>
+                        </div>
+                        <div className='image'>
+                            <img id={d.id} src={`https://cms.samespace.com/assets/${d.cover}`} alt={d.name} />
+                        </div>
                     </div>
-                    <div className='image'>
-                    <img id={d.id} src={`https://cms.samespace.com/assets/${d.cover}`} alt={d.name} />
-                    </div>
-                </div>
-                <div className='music--control'>
-                    <audio>
-                    <source src={d.url} type='audio/mp3' />
-                    </audio>
-                    <div class="player text-center">
-                    <button type="button" id="button_fbw" class="btn" onclick='buttonRewindPress()'>
-                    <i class="fa fa-fast-backward"></i>
-                    </button>
-                    
-                    <button type="button" id="button_bw" class="btn" onclick='buttonBackPress()'>
-                    <i class="fa fa-backward"></i>
-                    </button>
-                    
-                    <button type="button" id="button_play" class="btn" onclick='buttonPlayPress()'>
-                    <i class="fa fa-play"></i>
-                    </button>
-                    
-                    <button type="button" id="button_stop" class="btn" onclick='buttonStopPress()'>
-                    <i class="fa fa-stop"></i>
-                    </button>
-                    
-                    <button type="button" id="button_fw" class="btn" onclick='buttonForwardPress()'>
-                    <i class="fa fa-forward"></i>
-                    </button>
-                    
-                    <button type="button" id="button_ffw" class="btn" onclick='buttonFastforwardPress()'>
-                    <i class="fa fa-fast-forward"></i>
-                    </button>    
-                </div>
+                    <div className='music--control'>
+                        <audio ref={audioRef} onTimeUpdate={handleTimeUpdate}>
+                            <source src={d.url} type='audio/mp3' />
+                        </audio>
+
+                        <div class="range-container">
+                            <input type="range" class="range-input" min="0" max={audioRef.current ? audioRef.current.duration : 0} step="0.1" value={currentTime} />
+                        </div>
+
+                        <div class="music-controls">
+                            <button type="button" id="button_bw" class="btn" onClick={handleBackPress}>
+                                <i class="fas fa-backward"></i>
+                            </button>
+
+                            <button type="button" id="button_play" class="btn play-btn" onClick={handlePlayPause}>
+                                <i class={`fas ${isPlaying ? 'fa-pause' : 'fa-play'}`}></i>
+                            </button>
+
+                            <button type="button" id="button_fw" class="btn" onClick={handleForwardPress}>
+                                <i class="fas fa-forward"></i>
+                            </button>
+                        </div>
+
                     </div>
                 </div>
             ))}
-            </div>
-
-        )
+        </div>
+    );
 }
 
-export default Music
+export default Music;
--- samespaceui/src/components/Navbar/Navbar.js
+++ samespaceui/src/components/Navbar/Navbar.js
@@ -8,9 +8,13 @@ function Navbar( {searchData, setFilteredData } ) {
       setFilteredData(filterData);
   }
 
+  const handleForYou=()=>{
+    setFilteredData(searchData);
+  }
+
   return (
     <div className='navbar'>
-      <div className='navbar-item'>For You</div>
+      <div className='navbar-item' onClick={ handleForYou }>For You</div>
       <div className='navbar-item' onClick={ handletopTracks }>Top Tracks</div>
       <div className='navbar-item'>Favourites</div>
       <div className='navbar-item'>Recently Played</div>
